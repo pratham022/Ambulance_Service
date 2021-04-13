23a4cf92a9f40e1954d13cc65a9ed1d8ab08dbe7
pratham022 <pthombre657@gmail.com>
2021-04-13 22:50:18 +0530

    Added Directions to Map

--- app/src/main/java/com/example/ambulanceservice/MainActivity.java
+++ app/src/main/java/com/example/ambulanceservice/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.ambulanceservice;
 
 import android.annotation.SuppressLint;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,7 +16,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -35,12 +44,24 @@ import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.mapboxsdk.location.modes.RenderMode;
 
 import com.google.android.material.navigation.NavigationView;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 
 
 public class MainActivity extends AppCompatActivity implements
-        OnMapReadyCallback, PermissionsListener{
+        OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
 
 
     NavigationView nav;
@@ -50,16 +71,26 @@ public class MainActivity extends AppCompatActivity implements
     public MapboxMap mapboxMap;
     public MapView mapView;
 
+    private String TAG = "MainActivity";
+
 
     // Variables needed to handle location permissions
     private PermissionsManager permissionsManager;
     // Variables needed to add the location engine
     private LocationEngine locationEngine;
+    private LocationComponent locationComponent;
     private long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L;
     private long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
 // Variables needed to listen to location updates
     //Globally declare an instance of the class you created above:
     private MainActivityLocationCallback callback = new MainActivityLocationCallback(this);
+
+    // variables for calculating and drawing a route
+    private DirectionsRoute currentRoute;
+    private NavigationMapRoute navigationMapRoute;
+
+    // variables needed to initialize navigation
+    private Button button;
 
     /**
      * That's where the PermissionsManager class comes into play.
@@ -125,6 +156,23 @@ The permission result is invoked once the user decides whether to allow or deny 
 
     }
 
+
+
+    private void addDestinationIconSymbolLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addImage("destination-icon-id",
+                BitmapFactory.decodeResource(this.getResources(), R.drawable.mapbox_marker_icon_default));
+        GeoJsonSource geoJsonSource = new GeoJsonSource("destination-source-id");
+        loadedMapStyle.addSource(geoJsonSource);
+        SymbolLayer destinationSymbolLayer = new SymbolLayer("destination-symbol-layer-id", "destination-source-id");
+        destinationSymbolLayer.withProperties(
+                iconImage("destination-icon-id"),
+                iconAllowOverlap(true),
+                iconIgnorePlacement(true)
+        );
+        loadedMapStyle.addLayer(destinationSymbolLayer);
+    }
+
+
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap; //when map get's ready it passes mapboxMap instance
 
@@ -135,11 +183,64 @@ The permission result is invoked once the user decides whether to allow or deny 
                     public void onStyleLoaded(@NonNull Style style) {
 
                         enableLocationComponent(style);
+                        addDestinationIconSymbolLayer(style);
+
+                        mapboxMap.addOnMapClickListener(MainActivity.this);
+                        button = findViewById(R.id.startButton);
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                boolean simulateRoute = true;
+                                NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                        .directionsRoute(currentRoute)
+                                        .shouldSimulateRoute(simulateRoute)
+                                        .build();
+                                // Call this method with Context from within an Activity
+                                NavigationLauncher.startNavigation(MainActivity.this, options);
+                            }
+                        });
 
                     }
                 });
     }
 
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+
+                        currentRoute = response.body().routes().get(0);
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute.removeRoute();
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Log.e(TAG, "Error: " + throwable.getMessage());
+                    }
+                });
+    }
 
 
 
@@ -152,7 +253,7 @@ The permission result is invoked once the user decides whether to allow or deny 
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
 
 // Get an instance of the component
-            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent = mapboxMap.getLocationComponent();
 
 // Set the LocationComponent activation options
             //Retrieve and activate the LocationComponent once the user has granted location permission and the map has fully loaded.
@@ -303,4 +404,21 @@ The permission result is invoked once the user decides whether to allow or deny 
         mapView.onDestroy();
     }
 
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        Point destinationPoint = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+        Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                locationComponent.getLastKnownLocation().getLatitude());
+
+        GeoJsonSource source = mapboxMap.getStyle().getSourceAs("destination-source-id");
+        if (source != null) {
+            source.setGeoJson(Feature.fromGeometry(destinationPoint));
+        }
+
+        getRoute(originPoint, destinationPoint);
+        button.setEnabled(true);
+        button.setBackgroundResource(R.color.mapbox_blue);
+        return true;
+    }
 }
